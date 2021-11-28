edc4b6e0e4f704e21770a5cea15024ad956dc4d6
IgalGrin <89865761+IgalGrin@users.noreply.github.com>
2021-11-28 16:12:50 +0300

    2. Каркас шахматной доски [#792 #233516]

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -5,7 +5,7 @@ import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 
 public class BishopBlack implements Figure {
-    private final Cell position;
+    private final Cell position; /* клетка на которой создаем фигуру, координаты */
 
     public BishopBlack(final Cell ps) {
         position = ps;
@@ -18,13 +18,28 @@ public class BishopBlack implements Figure {
 
     @Override
     public Cell[] way(Cell dest) {
-        throw new ImpossibleMoveException(
-                String.format("Could not way by diagonal from %s to %s", position, dest)
-        );
+        if (!isDiagonal(position, dest)) {
+            throw new ImpossibleMoveException(
+                    String.format("Could not move by diagonal from %s to %s", position, dest)
+            );
+        }
+        int size = Math.abs(position.getX() - dest.getX());
+        Cell[] steps = new Cell[size]; /* массив вмещающий клетки от стартовой */
+                                       /* позиции фигуры до конечной без самой стартовой */
+        int deltaX = 1;
+        int deltaY = -1;
+        int x = position.getX();
+        int y = position.getY();
+        for (int index = 0; index < size; index++) {
+            x = x + deltaX;
+            y = y + deltaY;
+            steps[index] = Cell.findBy(x, y);
+        }
+       return steps;
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        return false;
+        return Math.abs(source.getX() - dest.getX()) == Math.abs(source.getY() - dest.getY());
     }
 
     @Override
--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -5,14 +5,47 @@ import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
 
+
+import static ru.job4j.chess.firuges.Cell.*;
+
 public class LogicTest {
 
     @Ignore
     @Test
     public void move()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        /* Создаем объект для доступа к методам класса Logic */
         Logic logic = new Logic();
+        /* Создали слона на С1, в массиве figures типа Figure */
         logic.add(new BishopBlack(Cell.C1));
+        /* Запускаем метод move c желаемыми параметрами */
         logic.move(Cell.C1, Cell.H6);
+    }
+
+    @Test(expected = ImpossibleMoveException.class)
+    public void whenIsNotDiagonial() {
+        new BishopBlack(C1).way(G6);
+    }
+
+    @Test(expected = FigureNotFoundException.class)
+    public void whenFigureNotFound() throws OccupiedCellException, FigureNotFoundException {
+        /* Фигура не найдена, т.к. мы ее не создавали */
+        Logic logic = new Logic();
+        /* У findBy доступ private, напрямую не обратиться, поэтому обращаемся через public move()*/
+        /* и ловим исключение */
+        logic.move(Cell.C1, H6);
+    }
+
+    @Test
+    public void whenFigureFound() throws OccupiedCellException, FigureNotFoundException {
+        Logic logic = new Logic();
+        /* Создаем фигуру в (нулевой) ячейке массива figures */
+        logic.add(new BishopBlack(Cell.C1));
+        /* запускаем findBy через public move()*/
+        logic.move(Cell.C1, H6);
+        /* Дальше нужно получить номер ячейки из int index, не знаю как, потом сравнить
+        через Assert.assertEquals()
+        Также нужно получить массив из Cell[] steps...Далее метод free... */
+
     }
 }
